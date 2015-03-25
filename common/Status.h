19436daccaf27bb3431ccb7dbd880239c0938371
silence <silencly07@gmail.com>
2015-03-26 00:09:32 +0800

    add status class

---
+++ common/Status.h
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Status.h
+ *
+ *    Description:  统一status封装
+ *
+ *        Version:  1.0
+ *        Created:  2015年03月26日 00时07分56秒
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Petrus (), silencly07@gmail.com
+ *   Organization:  http://www.codeplayer.org
+ *
+ * =====================================================================================
+ */
+
+class Status {
+
+}
