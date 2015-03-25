e20a0a411e1e44e69e627bbc36f3eb8644154a7e
silence <silencly07@gmail.com>
2015-03-25 23:14:10 +0800

    add master.h

---
+++ Master.h
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Master.cc
+ *
+ *    Description:  Game Server Master
+ *
+ *        Version:  1.0
+ *        Created:  2015年03月25日 23时10分55秒
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Petrus (), silencly07@gmail.com
+ *   Organization:  http://www.codeplayer.org
+ *
+ * =====================================================================================
+ */
+
+#ifndef MASTER_CC_XPRMPWDX
+#define MASTER_CC_XPRMPWDX
+
+class Master {
+    public:
+        Master();
+
+        virtual ~Master();
+
+}
+
+#endif /* end of include guard: MASTER_CC_XPRMPWDX */
